0d72c17793a92da0210145bbc351f196a829936b
LaVonda Gordon <raine1084@gmail.com>
2021-12-13 16:28:50 -0500

    finished tasks

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   Study the code for counter1 and counter2, then answer the questions below.
   
-  1. What is the difference between counter1 and counter2?
+  1. What is the difference between counter1 and counter2?Counter 2 is hoisted and counter 1 isn't.
   
-  2. Which of the two uses a closure? How can you tell?
+  2. Which of the two uses a closure? How can you tell? counter 2 because it can access information in counter 1
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
@@ -62,8 +62,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random() * 2)
 }
 
 
@@ -81,8 +81,14 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningcb, num){
+  const score = {
+    Home: inningcb(),
+    Away: inningcb()
+  }
+  for(let i = 0; i < 2; i++){
+return score;
+  }
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,10 +96,10 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+  return finalScore(inning, 1);
 }
-
+console.log(getInningScore(inning));
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
@@ -136,8 +142,17 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningScore, inningcb, number) {
+  const finalScore = [];
+  homeScore = 0;
+  awayScore = 0;
+  for(let i = 0; i < 2; i++){
+    let currentScore = getInningScore(inningcb);
+    homeScore = currentScore.home;
+    awayScore = currentScore.away;
+    finalScore.push(`Inning ${i + 1}: Home ${homeScore} - Away ${awayScore}`);
+  }
+  return finalScore;
 }
 
 
